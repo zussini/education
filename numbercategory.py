0571a2d43fb4aee70e407c91c2841015576e744d
zussini <zussini@gmail.com>
2024-02-15 21:18:47 +0100

    Create numbercategory.py

---
+++ numbercategory.py
@@ -0,0 +1,16 @@
+from manim import *
+
+class NumberCategory(Scene):
+    def construct(self):
+        # Creating dots for natural numbers
+        numbers = VGroup(*[Dot().shift(i*RIGHT) for i in range(5)])
+        labels = VGroup(*[Text(str(i)).next_to(numbers[i], DOWN) for i in range(5)])
+
+        # Creating arrows for addition morphisms
+        arrows = VGroup(
+            *[Arrow(numbers[i], numbers[i+1], buff=0.1) for i in range(4)]
+        )
+
+        self.play(Create(numbers), Write(labels))
+        self.play(Create(arrows))
+        self.wait(2)
